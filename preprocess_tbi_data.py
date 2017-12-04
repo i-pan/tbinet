2d94cf3629a9bafcc7f89e944400d6af43e4913a
David Liu <daviddliu@github.com>
2017-12-04 17:01:26 -0500

    hacked around preprocess_tbi_data to preprocess negative files

--- preprocess_tbi_data.py
+++ preprocess_tbi_data.py
@@ -23,6 +23,8 @@ sdh_seg_dir = "/users/ipan/scratch/tbi_data/slicer/sdh/gt/nii/"
 iph_seg_dir = "/users/ipan/scratch/tbi_data/slicer/iph/gt/nii/"
 cont_seg_dir = "/users/ipan/scratch/tbi_data/slicer/cont/gt/nii/"
 
+neg_data_dir = "/gpfs/data/dmerck/tbi_data/orig/neg/nii/"
+
 # Step 1: New directories
 # -----------------------
 resampled_edh_data = "/users/ipan/scratch/tbi_data/resampled/edh/ct/"
@@ -39,6 +41,8 @@ sampling_masks_edh = "/users/ipan/scratch/tbi_data/resampled/edh/masks/"
 sampling_masks_sdh = "/users/ipan/scratch/tbi_data/resampled/sdh/masks/"
 sampling_masks_iph = "/users/ipan/scratch/tbi_data/resampled/iph/masks/"
 sampling_masks_cont = "/users/ipan/scratch/tbi_data/resampled/cont/masks/"
+
+resampled_neg_data = "/gpfs/data/dmerck/tbi_data/resampled/neg/"
 
 # Step 2: New directories 
 # -----------------------
@@ -57,6 +61,9 @@ sampling_mask_sdh = "/users/ipan/scratch/tbi_data/final3/masks/sdh/"
 sampling_mask_iph = "/users/ipan/scratch/tbi_data/final3/masks/iph/"
 sampling_mask_cont = "/users/ipan/scratch/tbi_data/final3/masks/cont/"
 
+brain_neg_data = "/gpfs/data/dmerck/tbi_data/final3/bet/neg/"
+brain_neg_std_data = "/gpfs/data/dmerck/tbi_data/final3/bet/std/neg/"
+
 # Step 3: New directories
 # -----------------------
 edh_new_seg_dir = "/users/ipan/scratch/tbi_data/final3/gt/roi/edh/"
@@ -66,11 +73,18 @@ cont_new_seg_dir = "/users/ipan/scratch/tbi_data/final3/gt/roi/cont/"
 
 # ===== STEP 1 ===== 
 # Resample to 0.5 mm x 0.5 mm x 1.0 mm 
-def resample_everything(data_dir, seg_dir, new_data_out, new_seg_out):
+def resample_everything(data_dir, seg_dir, new_data_out, new_seg_out=None):
 	if not os.path.exists(new_data_out): os.makedirs(new_data_out)
-	if not os.path.exists(new_seg_out): os.makedirs(new_seg_out)
+	if new_seg_out:
+		if not os.path.exists(new_seg_out): os.makedirs(new_seg_out)
 	ct_files = check_output("ls " + data_dir, shell=True).split() 
-	gt_files = check_output("ls " + seg_dir, shell=True).split()
+	# Negative files have a different file structure.
+	if not new_seg_out:
+		ct_files = [directory+"/"+directory+".nii.gz" for directory in os.listdir(data_dir)]
+	if new_seg_out:
+		gt_files = check_output("ls " + seg_dir, shell=True).split()
+	else:
+		gt_files = []
 	skip = ("161","493","667","722","766","840","1055","1165","1254") 
 	for ct in ct_files:
 		if ct.split(".")[0] in skip: continue
@@ -79,6 +93,8 @@ def resample_everything(data_dir, seg_dir, new_data_out, new_seg_out):
 		ct_nifti = nib.load(os.path.join(data_dir, ct))
 		resampled_ct = resample_NIfTI(ct_nifti, [0.5,0.5,1.])
 		resampled_ct = rotate_180_degrees(resampled_ct) 
+		if not new_seg_out:
+			ct = ct.split("/")[1]
 		nib.save(resampled_ct, os.path.join(new_data_out, ct))
 		print "DONE in {} !".format(datetime.datetime.now() - start_time) 
 	for gt in gt_files: 
@@ -91,27 +107,32 @@ def resample_everything(data_dir, seg_dir, new_data_out, new_seg_out):
 		nib.save(resampled_gt, os.path.join(new_seg_out, gt))
 		print "DONE in {} !".format(datetime.datetime.now() - start_time) 
 
-resample_everything(edh_data_dir, edh_seg_dir, 
-	resampled_edh_data, resampled_edh_seg)
+#resample_everything(edh_data_dir, edh_seg_dir, 
+#	resampled_edh_data, resampled_edh_seg)
 
-resample_everything(sdh_data_dir, sdh_seg_dir, 
-	resampled_sdh_data, resampled_sdh_seg)
+#resample_everything(sdh_data_dir, sdh_seg_dir, 
+#	resampled_sdh_data, resampled_sdh_seg)
 
-resample_everything(iph_data_dir, iph_seg_dir, 
-	resampled_iph_data, resampled_iph_seg)
+#resample_everything(iph_data_dir, iph_seg_dir, 
+#	resampled_iph_data, resampled_iph_seg)
 
-resample_everything(cont_data_dir, cont_seg_dir, 
-	resampled_cont_data, resampled_cont_seg)
+#resample_everything(cont_data_dir, cont_seg_dir, 
+#	resampled_cont_data, resampled_cont_seg)
 
+# resample_everything(neg_data_dir, None, resampled_neg_data)
+
+	
 # ===== STEP 2 ===== 
 # a. Extract brain using FSL BET 
 # b. Max-pooling to 1 mm^3
 # c. Thresholding to [0, 100] HU 
 # d. Standardizing to zero mean, unary variance
-def finish_preprocessing(data_dir, new_data_out, new_mask_out, std_data_out): 
+def finish_preprocessing(data_dir, new_data_out, new_mask_out, std_data_out):
 	if not os.path.exists(new_data_out): os.makedirs(new_data_out)
-	if not os.path.exists(new_mask_out): os.makedirs(new_mask_out)
-	if not os.path.exists(std_data_out): os.makedirs(std_data_out)
+	if new_mask_out:
+		if not os.path.exists(new_mask_out): os.makedirs(new_mask_out)
+	if std_data_out:
+		if not os.path.exists(std_data_out): os.makedirs(std_data_out)
 	ct_files = check_output("ls " + data_dir, shell=True).split() 
 	for ct in ct_files:
 		if ct in ("493"): continue 
@@ -131,13 +152,14 @@ def finish_preprocessing(data_dir, new_data_out, new_mask_out, std_data_out):
 		nib.save(brain_ct, os.path.join(new_data_out, ct))
 		# Plan is to use bleed segmentation as sampling mask for positives
 		# But make this one anyways just in case you want to use it later
-		hu_threshold = 35 
-		print "Creating sampling mask using HU threshold {} ...".format(hu_threshold) 
-		sampling_mask = np.zeros_like(brain_ct.get_data())
-		sampling_mask[brain_ct.get_data() >= hu_threshold] = 1 
-		sampling_mask = nib.Nifti1Image(sampling_mask.astype("int16"), 
-			np.diag((1,1,1,1)))
-		nib.save(sampling_mask, os.path.join(new_mask_out, ct)) 
+		if new_mask_out:
+			hu_threshold = 35 
+			print "Creating sampling mask using HU threshold {} ...".format(hu_threshold) 
+			sampling_mask = np.zeros_like(brain_ct.get_data())
+			sampling_mask[brain_ct.get_data() >= hu_threshold] = 1 
+			sampling_mask = nib.Nifti1Image(sampling_mask.astype("int16"), 
+				np.diag((1,1,1,1)))
+			nib.save(sampling_mask, os.path.join(new_mask_out, ct)) 
 		print "Standardizing ..."
 		brain_mask = brain_ct.get_data().copy() 
 		brain_mask[brain_mask > np.min(brain_mask)] = 1
@@ -145,17 +167,19 @@ def finish_preprocessing(data_dir, new_data_out, new_mask_out, std_data_out):
 		nib.save(brain_ct, os.path.join(std_data_out, ct))
 		print "DONE in {} !".format(datetime.datetime.now() - start_time)
 
-finish_preprocessing(resampled_edh_data, brain_edh_data, 
-	sampling_mask_edh, brain_edh_std_data)
+#finish_preprocessing(resampled_edh_data, brain_edh_data, 
+#	sampling_mask_edh, brain_edh_std_data)
 
-finish_preprocessing(resampled_sdh_data, brain_sdh_data, 
-	sampling_mask_sdh, brain_sdh_std_data)
+#finish_preprocessing(resampled_sdh_data, brain_sdh_data, 
+#	sampling_mask_sdh, brain_sdh_std_data)
 
-finish_preprocessing(resampled_iph_data, brain_iph_data, 
-	sampling_mask_iph, brain_iph_std_data)
+#finish_preprocessing(resampled_iph_data, brain_iph_data, 
+#	sampling_mask_iph, brain_iph_std_data)
 
-finish_preprocessing(resampled_cont_data, brain_cont_data, 
-	sampling_mask_cont, brain_cont_std_data)
+#finish_preprocessing(resampled_cont_data, brain_cont_data, 
+#	sampling_mask_cont, brain_cont_std_data)
+
+finish_preprocessing(resampled_neg_data, brain_neg_data, None, brain_neg_std_data)
 
 # ==== STEP 3 ==== 
 # Edit the ground truth so that none of it lies outside of the extracted brain
@@ -182,17 +206,17 @@ def edit_ground_truth(data_dir, seg_dir, out_dir):
 			np.diag((1,1,1,1)))
 		nib.save(new_gt_nifti, os.path.join(out_dir, gt)) 
 
-edit_ground_truth(brain_edh_data, resampled_edh_seg, 
-	edh_new_seg_dir)
+#edit_ground_truth(brain_edh_data, resampled_edh_seg, 
+#	edh_new_seg_dir)
 
-edit_ground_truth(brain_sdh_data, resampled_sdh_seg, 
-	sdh_new_seg_dir)
+#edit_ground_truth(brain_sdh_data, resampled_sdh_seg, 
+#	sdh_new_seg_dir)
 
-edit_ground_truth(brain_iph_data, resampled_iph_seg, 
-	iph_new_seg_dir)
+#edit_ground_truth(brain_iph_data, resampled_iph_seg, 
+#	iph_new_seg_dir)
 
-edit_ground_truth(brain_cont_data, resampled_cont_seg, 
-	cont_new_seg_dir)
+#edit_ground_truth(brain_cont_data, resampled_cont_seg, 
+#	cont_new_seg_dir)
 
 	
 
@@ -203,15 +227,15 @@ import nibabel as nib, numpy as np
 from skimage.measure import label 
 from scipy.ndimage.filters import median_filter 
 
-x = nib.load("662.nii.gz")
-x = x.get_data() 
-x[x < 0] = 0 ; x[x > 100] = 0 
-x = median_filter(x, (4,4,2)) 
-bw = x > 20 
-y = label(bw, connectivity=2) 
-labels, counts = np.unique(y, return_counts=True) 
-labels, counts = labels[1:], counts[1:] 
-brain_label = labels[list(counts).index(np.max(counts))]
-z = x.copy() 
-z[y != brain_label] = np.min(z) 
-nib.save(nib.Nifti1Image(z, np.diag((1,1,1,1))), "test.nii.gz")
+#x = nib.load("662.nii.gz")
+#x = x.get_data() 
+#x[x < 0] = 0 ; x[x > 100] = 0 
+#x = median_filter(x, (4,4,2)) 
+#bw = x > 20 
+#y = label(bw, connectivity=2) 
+#labels, counts = np.unique(y, return_counts=True) 
+#labels, counts = labels[1:], counts[1:] 
+#brain_label = labels[list(counts).index(np.max(counts))]
+#z = x.copy() 
+#z[y != brain_label] = np.min(z) 
+#nib.save(nib.Nifti1Image(z, np.diag((1,1,1,1))), "test.nii.gz")
